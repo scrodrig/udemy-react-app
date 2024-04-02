bdbcda40cfa538e882bc5b396462652d7f11f0f2
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-02 17:13:06 +0100

    add counter state

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -1,7 +1,14 @@
 import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
+import { useState } from 'react';
 
 export const ProductCard = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increseBy = (value: number) => {
+    setCounter((prev) => Math.max(prev + value, 0));
+  };
+
   return (
     <div className={styles.productCard}>
       <img
@@ -12,9 +19,13 @@ export const ProductCard = () => {
       {/* <img className={styles.productImg} src={noImage} alt="coffe mug" /> */}
       <span className={styles.productDescription}>Coffee Mug</span>
       <div className={styles.buttonsContainer}>
-        <button className={styles.buttonMinus}> - </button>
-        <div className={styles.countLabel}> 0 </div>
-        <button className={styles.buttonAdd}> + </button>
+        <button className={styles.buttonMinus} onClick={() => increseBy(-1)}>
+          -
+        </button>
+        <div className={styles.countLabel}> {counter} </div>
+        <button className={styles.buttonAdd} onClick={() => increseBy(+1)}>
+          +
+        </button>
       </div>
     </div>
   );
