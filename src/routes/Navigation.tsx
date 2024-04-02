fe03395c19e4fa4d4e9ad98cac76dac4c68615be
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-02 12:26:05 +0100

    adds suspence component with loading fallback

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,10 +2,11 @@ import { BrowserRouter } from 'react-router-dom';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
 import logo from '../logo.svg';
 import { routes } from './routes';
+import { Suspense } from 'react';
 
 export const Navigation = () => {
   return (
-    <>
+    <Suspense fallback={<span>Loading... </span>}>
       <BrowserRouter>
         <div className="main-layout">
           <nav>
@@ -33,6 +34,6 @@ export const Navigation = () => {
           </Routes>
         </div>
       </BrowserRouter>
-    </>
+    </Suspense>
   );
 };
