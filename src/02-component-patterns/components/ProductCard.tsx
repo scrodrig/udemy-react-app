e9aa838f9e759ee06fafd33814d82f746e414f43
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-02 17:47:57 +0100

    fix types and provide a porduct for testing

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -2,18 +2,27 @@ import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
 import { useProduct } from '../hooks/useProduct';
 
-export const ProductCard = () => {
+interface Product {
+  id: string;
+  title: string;
+  img?: string;
+}
+
+interface Props {
+  product: Product;
+}
+
+export const ProductCard = ({ product }: Props) => {
   const { counter, increseBy } = useProduct(0);
 
   return (
     <div className={styles.productCard}>
       <img
         className={styles.productImg}
-        src="./coffee-mug.png"
+        src={product.img ? product.img : noImage}
         alt="coffe mug"
       />
-      {/* <img className={styles.productImg} src={noImage} alt="coffe mug" /> */}
-      <span className={styles.productDescription}>Coffee Mug</span>
+      <span className={styles.productDescription}>{product.title}</span>
       <div className={styles.buttonsContainer}>
         <button className={styles.buttonMinus} onClick={() => increseBy(-1)}>
           -
