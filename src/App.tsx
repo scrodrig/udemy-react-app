929148ff99359bd1d124c006222926951c1836cd
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-02 11:09:06 +0100

    Invoke routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Navigation } from './routes/Navigation'
 
 function App() {
-  return (
-    <>
-      <h1>App.tsx</h1>
-    </>
-  );
+    return (
+        <>
+            <Navigation />
+        </>
+    )
 }
 
-export default App;
+export default App
