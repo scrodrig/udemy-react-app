eaa30e425bc3e1ef3dd8ddb2801c91eadf8d12c2
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-03 15:41:49 +0100

    extract in different components

--- src/02-component-patterns/components/ProductCard.tsx
+++ src/02-component-patterns/components/ProductCard.tsx
@@ -12,26 +12,41 @@ interface Props {
   product: Product;
 }
 
+export const ProductImage = ({ img = '' }) => {
+  return <img className={styles.productImg} src={img ? img : noImage} alt="coffe mug" />;
+};
+
+export const ProductTitle = ({ title }: { title: string }) => {
+  return <span className={styles.productDescription}>{title}</span>;
+};
+
+interface ProductButtonsProps {
+  counter: number;
+  increaseBy: (value: number) => void;
+}
+
+export const ProductButtons = ({ counter, increaseBy }: ProductButtonsProps) => {
+  return (
+    <div className={styles.buttonsContainer}>
+      <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
+        -
+      </button>
+      <div className={styles.countLabel}> {counter} </div>
+      <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>
+        +
+      </button>
+    </div>
+  );
+};
+
 export const ProductCard = ({ product }: Props) => {
-  const { counter, increaseBy  } = useProduct(0);
+  const { counter, increaseBy } = useProduct(0);
 
   return (
     <div className={styles.productCard}>
-      <img
-        className={styles.productImg}
-        src={product.img ? product.img : noImage}
-        alt="coffe mug"
-      />
-      <span className={styles.productDescription}>{product.title}</span>
-      <div className={styles.buttonsContainer}>
-        <button className={styles.buttonMinus} onClick={() => increaseBy(-1)}>
-          -
-        </button>
-        <div className={styles.countLabel}> {counter} </div>
-        <button className={styles.buttonAdd} onClick={() => increaseBy(+1)}>
-          +
-        </button>
-      </div>
+      <ProductImage img={product.img} />
+      <ProductTitle title={product.title} />
+      <ProductButtons counter={counter} increaseBy={increaseBy} />
     </div>
   );
 };
