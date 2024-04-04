863cffd624a0cbbf4fb5a7015f3d6c0715f2435f
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-04 12:40:14 +0100

    use two states in a better way

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -1,51 +1,27 @@
-import { useState } from "react";
-import { ProductInCart, onChangeArgs } from "../interfaces/interfaces";
-
-
+import { useState } from 'react';
+import { ProductInCart, onChangeArgs } from '../interfaces/interfaces';
 
 export const useShoppingCart = () => {
- 
   const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
 
   const onProductCountChange = ({ count, product }: onChangeArgs) => {
     setShoppingCart((oldShoppingCartState) => {
-      const productInCart: ProductInCart = oldShoppingCartState[product.id] || {
-        ...product,
-        count: 0,
-      };
-
-      if (Math.max(productInCart.count + count, 0) > 0) {
-        productInCart.count += count;
+      if (count === 0) {
+        const { [product.id]: toDelete, ...rest } = oldShoppingCartState;
         return {
-          ...oldShoppingCartState,
-          [product.id]: productInCart,
+          ...rest,
         };
       }
 
-      //Borrar el producto
-
-      const { [product.id]: toDelete, ...rest } = oldShoppingCartState;
       return {
-        ...rest,
+        ...oldShoppingCartState,
+        [product.id]: { ...product, count },
       };
-
-      // if (count === 0) {
-      //   const { [product.id]: toDelete, ...rest } = oldShoppingCartState;
-      //   return {
-      //     ...rest,
-      //   };
-      // }
-
-      // return {
-      //   ...oldShoppingCartState,
-      //   [product.id]: { ...product, count },
-      // };
     });
   };
 
   return {
     shoppingCart,
-    onProductCountChange
-  }
-
-}
+    onProductCountChange,
+  };
+};
