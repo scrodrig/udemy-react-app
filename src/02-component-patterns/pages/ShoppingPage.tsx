2ba7af3f152df45ad5bbb5e472001960946468e5
Schubert Rodriguez Quezada <schubert.quezada@babelgroup.com>
2024-04-04 11:37:24 +0100

    add object entries map

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -62,25 +62,24 @@ export const ShoppingPage = () => {
       </div>
 
       <div className="shopping-cart">
-        <ProductCard style={{ width: '100px' }} product={product1} className="bg-dark text-white">
-          <ProductCard.Image
-            className="custom-image"
-            style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-          />
-          <ProductCard.Buttons className="custom-buttons" />
-        </ProductCard>
-
-        <ProductCard style={{ width: '100px' }} product={product2} className="bg-dark text-white">
-          <ProductCard.Image
-            className="custom-image"
-            style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-          />
-          <ProductCard.Buttons className="custom-buttons" />
-        </ProductCard>
-      </div>
-
-      <div>
-        <code>{JSON.stringify(shoppingCart, null, 5)}</code>
+        {Object.entries(shoppingCart).map(([key, product]) => {
+          return (
+            <ProductCard
+              key={key}
+              style={{ width: '100px' }}
+              product={product}
+              className="bg-dark text-white">
+              <ProductCard.Image
+                className="custom-image"
+                style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
+              />
+              <ProductCard.Buttons
+                className="custom-buttons"
+                style={{ display: 'flex', justifyContent: 'center' }}
+              />
+            </ProductCard>
+          );
+        })}
       </div>
     </div>
   );
